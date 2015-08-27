ff70abfa612c454e7024426a47055f470298c153
Adrian Roney <adrian.roney@tigerspike.com>
2015-08-27 13:43:26 +0400

    added button to welcom activity.
    And started Main Activity from it.

--- app/src/main/java/com/example/android/navigationdrawerexample/WelcomeActivity.java
+++ app/src/main/java/com/example/android/navigationdrawerexample/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.navigationdrawerexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -7,6 +8,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by anasabubacker on 27/08/2015.
@@ -23,6 +26,8 @@ public class WelcomeActivity extends FragmentActivity {
      */
     private ViewPager mPager;
 
+    private Button mNextButton;
+
     /**
      * The pager adapter, which provides the pages to the view pager widget.
      */
@@ -37,6 +42,15 @@ public class WelcomeActivity extends FragmentActivity {
         mPager = (ViewPager) findViewById(R.id.pager);
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
